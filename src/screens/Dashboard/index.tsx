39a04adbc8ea90510694fff368133281001cf83e
Denis Macedo <denisrmacedo@gmail.com>
2021-07-24 16:46:45 -0300

    create userCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Container, Title } from './styles'
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+ } from './styles'
 
 export function Dashboard(){
   return(
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/5804067?v=4'}} />
+            <User>
+              <UserGretting>Olá,</UserGretting>
+              <UserName>Denis</UserName>
+            </User>
+
+          </UserInfo>
+         </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,17 +1,54 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-font-family: ${({theme}) => theme.fonts.bold};
-  font-size: 24px;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  background-color: ${({theme}) => theme.colors.primary};
 `;
 
+export const UserWrapper = styled.View`
+  width: 100%;
+  background-color: red;
+  padding: 0 24px;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+  /* background-color: red; */
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+  /* background-color: red; */
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+  /* background-color: blue; */
+`;
+
+export const UserGretting = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.regular};
+  /* background-color: yellow; */
+`;
+
+export const UserName = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.bold};
+`;
 
