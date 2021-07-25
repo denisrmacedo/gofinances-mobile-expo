e3e85998092e18924a06e40198e9676b366fc85c
Denis Macedo <denisrmacedo@gmail.com>
2021-07-25 15:00:16 -0300

    add Highlightcard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 
 import { 
   Container,
@@ -7,27 +8,51 @@ import {
   UserInfo,
   Photo,
   User,
-  UserGretting,
+  UserGreeting,
   UserName,
   Icon,
+  HighlightCards,
  } from './styles'
 
 export function Dashboard(){
   return(
     <Container>
+      
       <Header>
         <UserWrapper>
           <UserInfo>
-            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/5804067?v=4'}} />
-            <User>
-              <UserGretting>Olá,</UserGretting>
-              <UserName>Denis</UserName>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/5804067?v=4'}}
+            />
+          <User>
+          <UserGreeting>Olá,</UserGreeting>
+            <UserName>Denis</UserName>
             </User>
           </UserInfo>
-          <Icon name="power"/>
-         </UserWrapper>
-         
-      </Header>
+          </UserWrapper>
+        </Header>
+
+          <HighlightCards>
+            <HighlightCard
+              type="up"
+              title="Entradas"
+              amount="1111"
+              lastTransaction="01 de janeiro"
+            />
+            <HighlightCard
+              type="down"
+              title="Saídas"
+              amount="222"
+              lastTransaction="02 de janeiro"
+            />
+            <HighlightCard
+              type="total"
+              title="Total"
+              amount="333"
+              lastTransaction="03 de janeiro"
+            />
+          </HighlightCards>
+
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -11,7 +12,7 @@ export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
   background-color: ${({theme}) => theme.colors.primary};
 `;
@@ -19,6 +20,7 @@ export const Header = styled.View`
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -39,7 +41,7 @@ export const User = styled.View`
   margin-left: 17px;
 `;
 
-export const UserGretting = styled.Text`
+export const UserGreeting = styled.Text`
   color: ${({theme}) => theme.colors.shape};
   font-size: ${RFValue(18)}px;
   font-family: ${({theme}) => theme.fonts.regular};
@@ -54,4 +56,14 @@ export const UserName = styled.Text`
 export const Icon = styled(Feather)`
   color: ${({theme}) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
